544aafa3b06a967678198dd7099fd7d5845ce73d
Diego Mozo <hezekiah001002@gmail.com>
2025-04-09 12:42:39 -0600

    Ya con las bbdd

--- databases/db.js
+++ databases/db.js
@@ -3,7 +3,11 @@ import pg from "pg";
 const Pool = pg.Pool;
 
 const pool = new Pool({
-
+    host: "localhost",
+    user: "postgres",
+    port: 5433,
+    password: "Chivas001002",
+    database: "listaUsuarios"
 });
 
 export default pool;
