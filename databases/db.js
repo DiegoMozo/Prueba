589a8f6f492602b38362155b9f0e13e8f1ed324b
Diego Mozo <hezekiah001002@gmail.com>
2025-04-13 21:49:56 -0600

    Corrección en URL de bases de datos

--- databases/db.js
+++ databases/db.js
@@ -1,13 +1,17 @@
 import pg from "pg";
+import dotenv from "dotenv";
 
+dotenv.config();
+
+const isProduction = process.env.NODE_ENV === "production";
 const Pool = pg.Pool;
+const PostgresURL = isProduction 
+    ? process.env.POSTGRES_URL
+    : process.env.LOCAL_POSTGRES_URL;
 
 const pool = new Pool({
-    host: "localhost",
-    user: "postgres",
-    port: 5433,
-    password: "Chivas001002",
-    database: "Usuarios"
+    connectionString: PostgresURL,
+    ssl: isProduction ? { rejectUnauthorized: false } : false
 });
 
 export default pool;
--- databases/mongodb.js
+++ databases/mongodb.js
@@ -1,9 +1,17 @@
 import { MongoClient } from "mongodb";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const isProduction = process.env.NODE_ENV === 'production';
+const MongoURL = isProduction 
+    ? process.env.MONGO_URL
+    : process.env.LOCAL_MONGO_URL;
 
 const conectMongo = async () => {
     try {
-        const mongoURL = "mongodb://0.0.0.0:27017/Profiles"
-        const client = await MongoClient.connect(mongoURL);
+        //const mongoURL = "mongodb://0.0.0.0:27017/Profiles"
+        const client = await MongoClient.connect(MongoURL);
         return client.db();
     } catch(err) {
         console.error(err);
